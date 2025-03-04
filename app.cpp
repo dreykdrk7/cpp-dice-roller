d05748ff9e7cfeb6220b4c0ae74e63c2fe45e8ff
Adrian Abad <adrianabadaledo@gmail.com>
2025-03-04 18:08:13 +0100

    dice rolls validation > 0

--- app.cpp
+++ app.cpp
@@ -9,6 +9,11 @@ int main() {
     std::cout << "Ingrese el número de tiradas del dado: ";
     std::cin >> numTiradas;
 
+    while (numTiradas <= 0) {
+        std::cout << "Por favor, ingrese un número válido de tiradas (mayor que 0): ";
+        std::cin >> numTiradas;
+    }
+
     for (int i = 0; i < numTiradas; i++) {
         int dado = std::rand() % 6 + 1;
         std::cout << "Tirada " << i + 1 << ": " << dado << std::endl;
