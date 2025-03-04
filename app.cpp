af1f0c9142a525b64395b54e246c5b07ce355a0c
Adrian Abad <adrianabadaledo@gmail.com>
2025-03-04 18:17:12 +0100

    Implemented an additional function to obtain the number of dice rolls and validate the input

--- app.cpp
+++ app.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <limits>
+#include <random>
+
+// Función para obtener un número entero válido del usuario
+int getValidInput() {
+    int numTiradas;
+    while (true) {
+        std::cout << "Ingrese el número de tiradas del dado: ";
+        std::cin >> numTiradas;
+
+        // Verificar si la entrada es un número entero válido
+        if (std::cin.fail() || numTiradas <= 0) {
+            std::cin.clear(); // Limpiar el estado de error
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Ignorar la entrada inválida
+            std::cout << "Por favor, ingrese un número entero válido (mayor que 0)." << std::endl;
+        } else {
+            break;
+        }
+    }
+    return numTiradas;
+}
 
 int main() {
     std::srand(std::time(0));
     
-    int numTiradas;
-    std::cout << "Ingrese el número de tiradas del dado: ";
-    std::cin >> numTiradas;
-
-    while (numTiradas <= 0) {
-        std::cout << "Por favor, ingrese un número válido de tiradas (mayor que 0): ";
-        std::cin >> numTiradas;
-    }
+    int numTiradas = getValidInput();
 
     for (int i = 0; i < numTiradas; i++) {
         int dado = std::rand() % 6 + 1;
